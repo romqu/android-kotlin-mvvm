9f49750654fe67ed459c10ab3061079941ffa7b0
romqu <roman.quistler@gmail.com>
2018-06-12 18:32:50 +0200

    push

--- app/src/main/java/de/sevennerds/trackdefects/data/AppJsonAdapterFactory.kt
+++
@@ -1,9 +0,0 @@
-package de.sevennerds.trackdefects.data
-
-/*
-@KotshiJsonAdapterFactory
-abstract class AppJsonAdapterFactory : JsonAdapter.Factory {
-    companion object {
-        val INSTANCE: AppJsonAdapterFactory = KotshiAppJsonAdapterFactory()
-    }
-}*/
