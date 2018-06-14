658c6c299ddf8da0b8e72f24fc68c8f4ea3448df
Niclas <niclas.kroon@protonmail.ch>
2018-06-14 18:50:14 +0200

    cleanup failed merge conflict

--- app/src/main/java/de/sevennerds/trackdefects/data/defect_list/DefectListEntity.kt
+++ app/src/main/java/de/sevennerds/trackdefects/data/defect_list/DefectListEntity.kt
@@ -7,21 +7,6 @@ import androidx.room.PrimaryKey
 import de.sevennerds.trackdefects.data.street_address.StreetAddressEntity
 
 @Entity(tableName = "defect_list")
-<<<<<<< HEAD
-data class DefectListEntity
-@JvmOverloads
-constructor(@PrimaryKey(autoGenerate = true) val id: Long,
-            @ColumnInfo(name = "remote_id") val remoteId: Long,
-            @ColumnInfo(name = "name") val name: String,
-            @ColumnInfo(name = "creation_date") val creationDate: String,
-            @Ignore val streetAddressEntity: StreetAddressEntity? = null)
-||||||| merged common ancestors
-data class DefectListEntity @JvmOverloads constructor(@PrimaryKey(autoGenerate = true) val id: Long,
-                                                      @ColumnInfo(name = "remote_id") val remoteId: Long,
-                                                      @ColumnInfo(name = "name") val name: String,
-                                                      @ColumnInfo(name = "creation_date") val creationDate: String,
-                                                      @Ignore val streetAddressEntity: StreetAddressEntity? = null)
-=======
 data class DefectListEntity @JvmOverloads constructor(
         @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Long,
         @ColumnInfo(name = "remote_id") val remoteId: Long,
@@ -29,4 +14,3 @@ data class DefectListEntity @JvmOverloads constructor(
         @ColumnInfo(name = "creation_date") val creationDate: String,
         @Ignore @ColumnInfo(name = "street_address_id") val streetAddressEntity: StreetAddressEntity? = null
 )
->>>>>>> enhancement/prettify-code
