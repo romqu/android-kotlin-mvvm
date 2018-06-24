3ca60863198e25c8a7ed413ba55df2226d837c3d
Niclas <niclas.kroon@protonmail.ch>
2018-06-24 23:32:45 +0200

    wh

--- app/src/main/java/de/sevennerds/trackdefects/data/floor_plan/FloorPlanEntity.kt
+++ app/src/main/java/de/sevennerds/trackdefects/data/floor_plan/FloorPlanEntity.kt
@@ -1,5 +1,21 @@
 package de.sevennerds.trackdefects.data.floor_plan
 
-data class FloorPlanEntity constructor(
-        val s: String
+import androidx.room.Entity
+import androidx.room.TypeConverters
+import de.sevennerds.trackdefects.common.Constants
+import de.sevennerds.trackdefects.data.PathTypeConverter
+
+@Entity(
+        tableName = "view_participant"
 )
+data class FloorPlanEntity constructor(
+        val fileName: String,
+        @TypeConverters(PathTypeConverter::class)
+        val path: Path
+) {
+    enum class Path() {
+        PHOTO_PATH(Constants.PHOTO_PATH),
+        FLOORPLAN_PATH(Constants.FLOOR_PLAN_PATH)
+    }
+}
+
